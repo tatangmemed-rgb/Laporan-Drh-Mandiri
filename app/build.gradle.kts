257ae47d19ec13acd08ffdb057c9109fddf3e968
tatangmemed-rgb <tatangmemed@gmail.com>
2026-08-21 18:45:36 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,28 +4,14 @@ plugins {
 }
 
 android {
-    namespace = "com.mediarta.laporandrhmandiri"
+    namespace = "com.mediarta.laporanhmandiri"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.mediarta.laporandrhmandiri"
+        applicationId = "com.mediarta.laporanhmandiri"
         minSdk = 23
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
-
-    buildFeatures {
-        compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
-    }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
 }
