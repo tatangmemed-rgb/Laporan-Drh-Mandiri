c2be17c81a1ccc2c66686c0d7e463080cec90b1c
tatangmemed-rgb <tatangmemed@gmail.com>
2026-08-21 18:56:58 +0700

    Update MainActivity

--- app/src/main/java/com/mediarta/laporandrhmandiri/MainActivity.kt
+++ app/src/main/java/com/mediarta/laporandrhmandiri/MainActivity.kt
@@ -1,23 +1,13 @@
 package com.mediarta.laporanhmandiri
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent {
-            MaterialTheme {
-                Surface {
-                    Text(text = "Laporan Dokter Hewan Mandiri")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
