fa8c768b97d28648d4b27ffc475f5bc45911817f
tatangmemed-rgb <tatangmemed@gmail.com>
2026-08-21 18:37:39 +0700

    Buat MainActivity

---
+++ app/src/main/java/com/mediarta/laporandrhmandiri/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.mediarta.laporanhmandiri
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MaterialTheme {
+                Surface {
+                    Text(text = "Laporan Dokter Hewan Mandiri")
+                }
+            }
+        }
+    }
+}
